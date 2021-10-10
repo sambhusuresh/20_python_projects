d4c634a8bf5bc315dd110b201f7f620832d8b7df
sambhusuresh <sambhusuresh4798@gmail.com>
2021-10-10 07:46:10 +0530

    ttt

---
+++ speech_to_text/main.py
@@ -0,0 +1,16 @@
+import pyttsx3
+import speech
+
+
+
+
+
+
+
+
+
+#incomplete due to dependency issues
+
+
+
+#pyttsx3 is not work properly in virtual env
--- text_to_speech/main.py
+++ text_to_speech/main.py
@@ -1,7 +1,9 @@
 import pyttsx3
 
-data = input("enter text you want to convert into speech")
+data = input("enter text you want to convert into speech:\n")
 
 engine = pyttsx3.init()
 engine.say(data)
 engine.runAndWait()
+
+#pyttsx3 is not work properly in virtual env
